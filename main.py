7c2cfa5e27fb428b009cfb1f9bbc5fb15871a95e
Eltaa <orcapeerawat@gmail.com>
2022-11-10 19:35:29 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+# from category import *
+# display setup
+WIDTH, HEIGHT = 800, 600
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+fps = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+def draw_window():
+    """ update game's look """
+    WINDOW.fill(BLACK)
+    pygame.display.update()
+
+# game loop
+
+
+def main():
+    ''' main function that run the game'''
+    tickspeed = pygame.time.Clock()
+    run = True
+    while run:
+        tickspeed.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        pygame.display.set_caption('Word Game')
+        draw_window()
+
+    pygame.quit
+
+
+if __name__ == '__main__':
+    main()
